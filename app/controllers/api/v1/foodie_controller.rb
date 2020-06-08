b9fd99804c382483062da8b6b1784629416c5a58
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-08 10:32:06 -0500

    Work in progress

---
+++ app/controllers/api/v1/foodie_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::FoodieController < ApplicationController
+
+  def show
+    
+  end
+
+end
--- spec/requests/api/v1/foodie_spec.rb
+++ spec/requests/api/v1/foodie_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'As a user' do
   describe 'can get foodie data' do
     it 'can get foodie info based on location' do
 
-    get "/api/v1/foodie?location=denver,co"
+    get "/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian"
     expect(response).to be_successful
     json_response = JSON.parse(response.body)
-
+    binding.pry
     expect(json_response['data']['attributes']).to have_key("end_location")
     expect(json_response['data']['attributes']).to have_key("travel_time")
     expect(json_response['data']['attributes']).to have_key("forecast")
