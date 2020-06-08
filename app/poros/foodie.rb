5d4b919ff0fbb69e97043d7317cd6ac937b7329a
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-08 12:17:46 -0500

    work in progress

---
+++ app/poros/foodie.rb
@@ -0,0 +1,10 @@
+class Foodie
+
+  def initialize(end_location, travel_time, forecast, restaurant)
+    @end_location = end_location
+    @travel_time = travel_time
+    @forecast = forecast
+    @restaurant = restaurant
+  end
+
+end
--- app/poros/foodie_facade.rb
+++ app/poros/foodie_facade.rb
@@ -6,24 +6,32 @@ class FoodieFacade
     @cuisine = cuisine
     @entity_id = get_entity[:location][:entity_id]
     @restaurant_info = get_restaurant
+    @google_service = GoogleService.new.get_distance(start, ending)
+    binding.pry
+    @travel_time = get_travel_time
   end
 
   def get_entity
-    google_service = GoogleService.new.get_distance(@start, @ending)
     entity_response = Faraday.get('https://developers.zomato.com/api/v2.1/geocode?') do |faraday|
       faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
-      faraday.params['lat'] = google_service['routes'][0]['legs'][0]['end_location']['lat']
-      faraday.params['lon'] = google_service['routes'][0]['legs'][0]['end_location']['lng']
+      faraday.params['lat'] = @google_service['routes'][0]['legs'][0]['end_location']['lat']
+      faraday.params['lon'] = @google_service['routes'][0]['legs'][0]['end_location']['lng']
     end
     JSON.parse(entity_response.body, symbolize_names: true)
   end
+
   def get_restaurant
     restaurant_response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{@entity_id}&entity_type=subzone&q=#{@cuisine}") do |faraday|
       faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
     end
-    restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
-    restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
+    parsed_restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
+    parsed_restaurant_info[:restaurants][0]
+  end
+
+  def get_travel_time
+    @google_service['routes'][0]['legs'][0]['duration']['text']
     binding.pry
   end
+  # restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
 
 end
