a04b07b041f141179ea82931ab0fd016a3a16b36
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-08 12:03:16 -0500

    work in progress

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,20 +1,21 @@
 class Api::V1::FoodieController < ApplicationController
 
   def show
-    google_service = GoogleService.new.get_distance(params[:start], params[:end])
-    entity_response = Faraday.get('https://developers.zomato.com/api/v2.1/geocode?') do |faraday|
-      faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
-      faraday.params['lat'] = google_service['routes'][0]['legs'][0]['end_location']['lat']
-      faraday.params['lon'] = google_service['routes'][0]['legs'][0]['end_location']['lng']
-    end
-    entity_info = JSON.parse(entity_response.body, symbolize_names: true)
-    restaurant_response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{entity_info[:location][:entity_id]}&entity_type=subzone&cuisines=55") do |faraday|
-      faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
-    end
-    restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
-    restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
-    restaurant_address = 
-    binding.pry
+    foodie = FoodieFacade.new(params[:start], params[:end], params[:search])
+    # google_service = GoogleService.new.get_distance(params[:start], params[:end])
+    # entity_response = Faraday.get('https://developers.zomato.com/api/v2.1/geocode?') do |faraday|
+    #   faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
+    #   faraday.params['lat'] = google_service['routes'][0]['legs'][0]['end_location']['lat']
+    #   faraday.params['lon'] = google_service['routes'][0]['legs'][0]['end_location']['lng']
+    # end
+    # entity_info = JSON.parse(entity_response.body, symbolize_names: true)
+    # restaurant_response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{entity_info[:location][:entity_id]}&entity_type=subzone&cuisines=55") do |faraday|
+    #   faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
+    # end
+    # restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
+    # restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
+    # restaurant_address =
+    # binding.pry
   end
 
 end
--- app/poros/foodie_facade.rb
+++ app/poros/foodie_facade.rb
@@ -1,18 +1,29 @@
 class FoodieFacade
 
-  google_service = GoogleService.new.get_distance(params[:start], params[:end])
-  entity_response = Faraday.get('https://developers.zomato.com/api/v2.1/geocode?') do |faraday|
-    faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
-    faraday.params['lat'] = google_service['routes'][0]['legs'][0]['end_location']['lat']
-    faraday.params['lon'] = google_service['routes'][0]['legs'][0]['end_location']['lng']
+  def initialize(start, ending, cuisine)
+    @start = start
+    @ending = ending
+    @cuisine = cuisine
+    @entity_id = get_entity[:location][:entity_id]
+    @restaurant_info = get_restaurant
   end
-  entity_info = JSON.parse(entity_response.body, symbolize_names: true)
-  restaurant_response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{entity_info[:location][:entity_id]}&entity_type=subzone&q=#{params[:search]}") do |faraday|
-    faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
+
+  def get_entity
+    google_service = GoogleService.new.get_distance(@start, @ending)
+    entity_response = Faraday.get('https://developers.zomato.com/api/v2.1/geocode?') do |faraday|
+      faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
+      faraday.params['lat'] = google_service['routes'][0]['legs'][0]['end_location']['lat']
+      faraday.params['lon'] = google_service['routes'][0]['legs'][0]['end_location']['lng']
+    end
+    JSON.parse(entity_response.body, symbolize_names: true)
   end
-  restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
-  restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
-  binding.pry
-  restaurant_address =
+  def get_restaurant
+    restaurant_response = Faraday.get("https://developers.zomato.com/api/v2.1/search?entity_id=#{@entity_id}&entity_type=subzone&q=#{@cuisine}") do |faraday|
+      faraday.headers['user-key'] = ENV['ZOMATO_API_KEY']
+    end
+    restaurant_info = JSON.parse(restaurant_response.body, symbolize_names: true)
+    restaurant_name = restaurant_info[:restaurants][0][:restaurant][:name]
+    binding.pry
+  end
 
 end
