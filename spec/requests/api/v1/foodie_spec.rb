5bbfe107de2eabc35ce9b771cefe2aa108f75c0a
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-08 10:16:43 -0500

    Work in progress

---
+++ spec/requests/api/v1/foodie_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'can get foodie data' do
+    it 'can get foodie info based on location' do
+
+    get "/api/v1/foodie?location=denver,co"
+    expect(response).to be_successful
+    json_response = JSON.parse(response.body)
+
+    expect(json_response['data']['attributes']).to have_key("end_location")
+    expect(json_response['data']['attributes']).to have_key("travel_time")
+    expect(json_response['data']['attributes']).to have_key("forecast")
+    expect(json_response['data']['attributes']).to have_key("restaurant")
+    end
+  end
+end
