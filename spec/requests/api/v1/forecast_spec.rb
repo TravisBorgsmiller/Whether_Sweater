db424069f57ab315ca0117c1739a3aa3aaa21aee
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-08 09:54:41 -0500

    add test

---
+++ spec/requests/api/v1/forecast_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'can get forecast data' do
+    it 'can get forecast based on location' do
+
+      get "/api/v1/forecast?location=denver,co"
+      expect(response).to be_successful
+      json_response = JSON.parse(response.body)
+      expect(json_response['data']['attributes']).to have_key("current_weather")
+      expect(json_response['data']['attributes']).to have_key("hourly_weather")
+      expect(json_response['data']['attributes']).to have_key("five_day_forecast")
+      expect(json_response['data']['attributes']).to have_key("time_and_date")
+    end
+  end
+end
