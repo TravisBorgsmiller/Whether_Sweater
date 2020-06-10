976397e8ba2e4aa3984b5acc688e21043f8aec0e
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-10 09:51:02 -0500

    add location to poro

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     forecast_search = ForecastFacade.new(params[:location])
-    forecast = Forecast.new(forecast_search.get_forecast)
+    forecast = Forecast.new(forecast_search.get_forecast, params[:location])
     render json: ForecastSerializer.new(forecast)
   end
 
