cda7e429ea51013272ad6c658f8fa03799a2d89d
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-06-10 09:50:23 -0500

    update serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,4 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :current_weather, :hourly_weather, :five_day_forecast, :time_and_date
+  attributes :id, :current_weather, :hourly_weather, :five_day_forecast, :time_and_date, :location
 end
